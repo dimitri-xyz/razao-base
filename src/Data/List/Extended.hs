3508eed738b9d61922ccd6a3c399e8602b6a8a13
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2017-06-23 17:18:32 -0600

    Further extend Data.List
    This adds `elem'`, `isSubsetOf` and `isEquivTo`. I don't see how to
    get better asymptotics for the latter two without adding class
    constraints.

--- src/Data/List/Extended.hs
+++ src/Data/List/Extended.hs
@@ -2,6 +2,9 @@ module Data.List.Extended
  ( module Data.List
  , safeHead
  , defaultHead
+ , elem'
+ , isSubsetOf
+ , isEquivTo
  ) where
 
 import Data.List
@@ -13,3 +16,16 @@ safeHead  (a:_) = Just a
 defaultHead :: a -> [a] -> a
 defaultHead  d []    = d
 defaultHead  _ (h:_) = h
+
+-- Generalizations (to distinct types a and b) from Data.List
+elem' :: Foldable t => (a -> b -> Bool) -> a -> t b -> Bool
+elem' f a = any (f a)
+
+-- This is O(N*M), but it will have to do for now
+isSubsetOf :: (a -> b -> Bool) -> [a] -> [b] -> Bool
+isSubsetOf _   []     _      = True
+isSubsetOf iso (a:as) bs = elem' iso a bs && isSubsetOf iso as bs
+
+-- O(N*M) asymptotic complexity
+isEquivTo :: ( a -> b -> Bool) -> [a] -> [b] -> Bool
+isEquivTo iso as bs = isSubsetOf iso as bs && isSubsetOf (flip iso) bs as
